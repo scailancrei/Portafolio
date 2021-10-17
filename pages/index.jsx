c41c841a494d32cc316c9daddf3f4983383a6d32
Juan Antonio <juanantonio_genial@hotmail.com>
2021-10-17 23:07:56 +0200

    portada

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,70 @@
 import Layout from './Components/Layout/layout'
+import Image from 'next/image'
 import styled, { keyframes } from 'styled-components';
-import { bounceIn } from 'react-animations';
+import { bounceIn, fadeInLeft } from 'react-animations';
 import { Container, Button } from 'react-bootstrap'
 import { useState } from 'react'
 
 
+const bounceAnimation = keyframes`${bounceIn}`
 
-const Index = () => {
+const BouncyDiv = styled.div`
+    animation: 2s ${bounceAnimation};
+    `
 
-    const [estado, setEstado] = useState(true)
-    const bounceAnimation = keyframes`${bounceIn}`
+const fadeInAnimation = keyframes`${fadeInLeft}`
 
-    const BouncyDiv = styled.div`
-      animation: 3s ${bounceAnimation};`
+const FadeDiv = styled.div`
+    animation: 5s ${fadeInAnimation};
+    animation-delay: 3;
+  `
 
 
-    function Portada() {
-        return (
+
+
+//La portada de la web al entrar
+//Si se pulsa el botón de entrar cambia el estado de la portada a false y entramos en el layout
+const Portada = ({ cambiarEstado }) => {
+
+    return (
+
+        <Container fluid style={{ display: 'flex', justifyContent: 'space-around' }}>
+            
+                <FadeDiv>
+                    <Image src={'/mi foto.jpg'} width={'100px'} height={'100px'} ></Image>
+                </FadeDiv>
+            
             <BouncyDiv>
-                <Container fluid style={{ textAlign: 'center', margin: '20px' }}>
-                    <h1>Hola me llamo Juan Antonio Pérez Lores</h1>
-                    <p>Tengo 25 años y soy de Almería.</p>
-                    <Button variant="primary" onClick={() => setEstado(!estado)}>Entrar</Button>
-                </Container>
+                <h1>Hola me llamo Juan Antonio Pérez Lores</h1>
+                <p>Tengo 25 años y soy de Almería.</p>
+
+                <Button variant="primary" onClick={cambiarEstado}>Entrar</Button>
             </BouncyDiv>
 
-        )
-    }
 
+
+        </Container>
+
+
+    )
+}
+
+
+//Componente principal de Index donde se gestiona el estado de la portada y el layout.
+const Index = () => {
+
+    const [estado, setEstado] = useState(false)
+
+    const handleEstado = () => {
+        setEstado(!estado)
+    }
 
     return (
         <>
 
-
-
             {estado
-                ? <Portada />
-                : <Layout />
+                ? <Layout />
+                : <Portada cambiarEstado={handleEstado} />
             }
 
 
